a1917661f0d819facbc74da553bd096a3ff5e1cb
master-yi <2051554040@qq.com>
2014-05-14 17:19:33 +0800

    第一阶段版

---
+++ README.txt
@@ -0,0 +1,6 @@
+本范例使用
+Eclipse Java EE IDE for Web Developers Kepler
+Tomcat7
+使用前请务必保证Eclipse字符集设置为utf-8，
+window-preferences-general-workspace中的
+text-file-encoding设置为utf-8
---
+++ src/com/ceit/web/AppController1.java
@@ -0,0 +1,37 @@
+package com.ceit.web;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AppController1 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH：mm：ss");		
+		
+		//将实时生成的数据——也就是时间存入request对象中
+		request.setAttribute("buildtime", sdf.format(new Date()));
+		//使用requestDispacher对象将request转发到result.jsp中
+		request.getRequestDispatcher("result.jsp").forward(request, response);
+		
+		/*以下的代码将被移动到JSP代码去完成*/
+//		response.setContentType("text/html;charset=utf-8");// <b>必须</b>，规定输出文件类型和字符集
+//		PrintWriter pw = response.getWriter();// 获得输出流
+//		pw.println("<h1>你好！世界</h1>");// 在输出流中写入数据
+//		pw.println("<p>这个页面很简单，但正如大家所见，它不是预先写好并以html文本的形式存放于服务器，而是由servlet生成的，每发送一次请求就生成一次，这就是所谓<b>动态</b>页面。</p>");
+//		pw.println("<p>这两种方式的区别就像食堂的大锅菜和小炒一样，后者可以根据不同的要求进行处理,获得不同的效果</p>");
+//		pw.println("<p>例如，以下的页面生成时间就是实时获得，不可能预先写好</p>");
+//		pw.println("<p>本页面生成时间为："+sdf.format(new Date())+"</p>");
+//		pw.close();// <b>必须</b>，关闭输出流
+
+	}
+}
